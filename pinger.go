5a6b3dfeb81ca02f2dc0959b8a0ea05aa6fc0734
Parikshit Misra <perryizgr8@live.in>
2020-12-09 00:23:34 +0530

    Initial commit

---
+++ pinger.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/go-ping/ping"
+	"os"
+	"sync"
+	"time"
+)
+
+func main() {
+	f, _ := os.Open("list.txt")
+	scanner := bufio.NewScanner(f)
+	var waitGroup sync.WaitGroup
+	for scanner.Scan() {
+		line := scanner.Text()
+		waitGroup.Add(1)
+		go func() {
+			fmt.Printf("pinging %s\n", line)
+			pinger, err := ping.NewPinger(line)
+			if err != nil {
+				panic(err)
+			}
+			pinger.Count = 3
+			pinger.Timeout = time.Second * 10
+			err = pinger.Run()
+			if err != nil {
+				panic(err)
+			}
+			stats := pinger.Statistics()
+			fmt.Println(stats.Addr)
+			fmt.Printf("tx=%d, rx=%d\n", stats.PacketsSent, stats.PacketsRecv)
+			fmt.Printf("min=%d, max=%d, avg=%d\n", stats.MinRtt, stats.MaxRtt, stats.AvgRtt)
+			waitGroup.Done()
+		}()
+	}
+	waitGroup.Wait()
+}
